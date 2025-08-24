92af56b89170ea26cb0cbf48a7ac3a4fb4955fb5
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-08-24 12:49:41 +0200

    w-screen

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -2,7 +2,7 @@ import { Quote, QuoteCard } from "@/components/card";
 
 export default function QuoteList({ quotes }: { quotes: Quote[] }) {
   return (
-    <div className="w-full max-w-full mx-auto px-4 sm:px-8 columns-1 md:columns-3 gap-6 mt-4">
+    <div className="w-screen max-w-full mx-auto px-4 sm:px-8 columns-1 md:columns-3 gap-6 mt-4">
       {quotes.map((q) => (
         <div key={q.id} className="mb-6 break-inside-avoid">
           <QuoteCard quote={q} />
