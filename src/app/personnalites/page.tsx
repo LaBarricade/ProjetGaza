d57bae219624a135cc6b3af436f3c29af05d9b7d
Benoit <benoit.guchet@gmail.Com>
2026-01-20 11:42:36 +0100

    Passage contact/mentions/personalites en SSR

--- src/app/personnalites/page.tsx
+++ src/app/personnalites/page.tsx
@@ -1,12 +1,12 @@
-"use client";
 
-import { useCallback, useEffect, useState } from "react";
+//import { useCallback, useEffect, useState } from "react";
 import { PersonalityList } from "@/components/list/personality-list";
 import {Personality} from "@/types/Personality";
 import {callLocalApi} from "@/lib/backend/api-client";
+import {getDbService} from "@/lib/backend/db-service";
 
-export default function PersonalitiesPage() {
-  const [data, setData] = useState<Personality[] | null>(null);
+export default async function PersonalitiesPage() {
+  /*const [data, setData] = useState<Personality[] | null>(null);
   const [filteredResults] = useState<Personality[] | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -28,19 +28,23 @@ export default function PersonalitiesPage() {
       await fetchData();
     };
     fetchDataAsync();
-  }, []);
+  }, []);*/
+
+  const resp = await getDbService().findPersonalities({});
+  const items = resp.items;
+
 
   return (
     <main className="flex flex-1 flex-col items-center w-full px-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl mx-auto">
-      {loading && (
+      {/*loading && (
         <div className="flex flex-1 items-center h-full">
           <p>Chargement des données...</p>
         </div>
-      )}
+      )*/}
 
-      {!filteredResults && data && data.length > 0 && <PersonalityList personalities={data} />}
+      {items && items.length > 0 && <PersonalityList personalities={items} />}
 
-      {data && data.length === 0 && (
+      {items && items.length === 0 && (
         <div className="flex flex-1 items-center h-full">
           <p>Aucun résultat trouvé.</p>
         </div>
