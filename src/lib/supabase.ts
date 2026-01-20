9ec6145b9f9b63e4fa99bd8fa7f8898965759765
Benoit <benoit.guchet@gmail.Com>
2026-01-20 10:59:12 +0100

    Fix scope identifiants Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 if (!supabaseUrl || !supabaseKey)
     throw new Error('No supabase url or key. Create .env.local file with supabase url and key')
