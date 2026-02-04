9305d75776d2db45117af6a7491e4fe166c4c8c6
Benoit <benoit.guchet@gmail.Com>
2026-02-04 20:29:33 +0100

    Page à propos : premier texte

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -1,6 +1,57 @@
 export default function AboutPage() {
     return <div className="mx-auto p-6 w-2xl ">
         <h1 className="text-3xl font-bold mb-2">À propos</h1>
-        <p></p>
+<div className="html-text">
+        <h2>Qu’est-ce que c’est la boussole ?</h2>
+        <p>La boussole est un <strong>outil citoyen indépendant</strong> qui vise à rendre compte des positionnements et des biais
+            idéologiques des élus locaux, nationaux et européens. À travers la collecte et l’analyse de leurs
+            déclarations publiques et de leurs votes dans les différentes assemblées politiques, la boussole entend
+            mieux informer le citoyen français sur le positionnement réel des élus à qui il confie son pouvoir de
+            représentation.</p>
+        <h2>Pourquoi la Boussole ?</h2>
+        <p>À l’ère de la post-vérité, où les travaux scientifiques et le droit sont remplacés par des opinions, où le
+            journalisme est invisibilisé au profit de l’éditorialisme, il est essentiel de garder le cap des principes
+            fondamentaux qui ont fondé les sociétés démocratiques. Pour ne pas perdre le nord, il faut continuer de
+            considérer le droit (à l’autodétermination, international, humanitaire) comme notre boussole, car l’état de
+            droit est constitutif de la démocratie.</p>
+        <p>L’autre valeur fondamentale de la boussole est la redevabilité des élus envers les citoyens qui leur ont
+            confié un mandat de représentation. C’est rendre compte et être imputable des actions et des paroles au
+            cours d’un mandat qui leur a été confié par le peuple. Mentir dans le débat public, soutenir des entorses au
+            droit national ou international, défendre des intérêts qui ne sont pas ceux des citoyens nationaux sont
+            autant d’atteintes à la démocratie représentative et à la souveraineté populaire. Par la collecte et
+            l’analyse des paroles et des votes de nos élus, la boussole entend rendre publiques les ambiguïtés ou les
+            contradictions entre paroles et actes afin que chaque citoyen puisse juger du bon exercice de sa
+            délégation.</p>
+        <h2>Pourquoi Gaza comme critère de redevabilité ?</h2>
+        <p>Le choix du conflit israélo-palestinien comme critère de redevabilité des élus français nous semble pertinent
+            d’un point de vue idéologique, car il permet de révéler des postures et des contradictions aussi bien à
+            l’échelle nationale qu’internationale. Il est un marqueur du double standard qui peut exister entre les
+            valeurs démocratiques et universelles qu’un élu déclare défendre et les actions ou les votes concrets qu’il
+            effectue.</p>
+        <p>De ce point de vue, le conflit israélo-palestinien est symptomatique de principes et de valeurs et permet
+            d’éclairer les positionnements de nos élus. Comment en effet soutenir les droits humains et
+            l’autodétermination des peuples en général, mais pas les droits des Palestiniens en particulier ? Il est
+            également symptomatique, car il s’agit d’un conflit colonial, et le nier ou situer le début du conflit au 7
+            octobre 2023 est en filigrane un révélateur idéologique du rapport de l’élu au droit et à son application
+            universelle. Participer, par ailleurs, en tant qu’élu à l’importation de ce conflit en France, en relayant
+            la thèse du choc des civilisations, en instrumentalisant l’antisémitisme ou en criminalisant le soutien
+            légitime aux droits des Palestiniens sont des positionnements problématiques dont il faut rendre compte.</p>
+        <p>Parce que la France est un soutien actif d’Israël et normalise un système illégal d’occupation par ses choix
+            politiques, économiques, militaires et diplomatiques, parce qu’elle comprend les plus importantes
+            communautés
+            juive et musulmane d’Europe, parce qu’elle est une ancienne puissance coloniale, la thématique est clivante.
+            Ce conflit est de ce fait la boussole morale du monde. C’est l’histoire de 75 ans d’injustices et de
+            dépossession et peut-être du conflit le plus injuste, le plus brutal et le plus déséquilibré sur une aussi longue
+            période.
+            C’est aussi un conflit colonial avec l’assentiment des États (mais pas des peuples) qui accorde une impunité
+            totale à Israël en totale contradiction avec le droit international. Pour toutes ces raisons, nous pensons que
+            cette thématique est un bon critère de redevabilité des élus.</p>
+        <h2>Quels sont nos objectifs</h2>
+        <p>La boussole entend <strong>informer le citoyen, la société civile et les médias</strong> sur le positionnement des élus en
+            France.
+            <strong>Archiver, quantifier, sourcer</strong> les paroles de ceux qui participent à l’affaiblissement de l’état de droit et
+            à son caractère universel. <strong>Constituer des preuves</strong> dans la constitution d’éventuels dossiers juridiques au
+            moment où la reconnaissance d’un génocide à Gaza sera officiellement établie.</p>
+</div>
     </div>;
 }
